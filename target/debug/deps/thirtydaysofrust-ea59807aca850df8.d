a56b3b13470b24bfbcbc547346e9566ef4863e51
raptisj <johnsitpar@gmail.com>
2020-08-09 20:39:03 +0300

    new folder

--- target/debug/deps/thirtydaysofrust-ea59807aca850df8.d
+++
@@ -1,5 +0,0 @@
-/home/john/Documents/code/thirtydaysofrust/target/debug/deps/thirtydaysofrust-ea59807aca850df8: src/main.rs
-
-/home/john/Documents/code/thirtydaysofrust/target/debug/deps/thirtydaysofrust-ea59807aca850df8.d: src/main.rs
-
-src/main.rs:
--- target/debug/thirtydaysofrust.d
+++
@@ -1,1 +0,0 @@
-/home/john/Documents/code/thirtydaysofrust/target/debug/thirtydaysofrust: /home/john/Documents/code/thirtydaysofrust/src/main.rs
